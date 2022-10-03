1f2f052f8bd6a12fcb8f199d7194dfd3fd552d85
kepop1 <kepop123@gmail.com>
2022-10-03 11:45:00 +0100

    Make it room based - first iteration

--- be/server.js
+++ be/server.js
@@ -3,29 +3,36 @@ const app = express();
 const http = require("http");
 const cors = require("cors");
 const server = http.createServer(app);
-
 const { Server } = require("socket.io");
-
+const bcrypt = require("bcrypt");
 /*
   DONE Get user ids and frontends connecting to some kind of store
   DONE Make the FE clear the tracker and display the players
   DONE Store by the player's name instead as player object
   DONE Make the one's who disconnect dissapear
-  Make it room based for a group
-  Make it so a DM can select who has the order etc.
-  Ability to clear all the users out
+  DONE Make it so it actually has rolls in the list - remove the uuid
+  DONE - Make it room based for a group with owner and players
+  DONE - Make it room based for a group with owner and players
+  DONE Allow the people to leave a room
+  Update rooms when someone disconnects
+  Make it sort based on initiativeRoll
+  Button to toggle the order / cycle through who's turn it is
+  Should be able to change a room's password
+  Owner should be able to add NPC's to initiative list
+  Allow initiative roll to update for a new encounter
+  Ability to clear all the users out of a room
 */
 
 // SocketIO server has to be setup with an origin for CORS - see docs
 const io = new Server(server, {
   cors: {
-    origin: "http://127.0.0.1:5173",
+    origin: "http://localhost:5173",
   },
 });
 
-// This is our 'db' for now
-const currentPlayers = [];
-
+// The new DB for now
+let rooms = [];
+console.log(rooms);
 // Setup CORS for other endpoints if we end up using them
 app.use(cors());
 
@@ -36,28 +43,184 @@ io.on("connection", (socket) => {
   // Each connection has it's own socket id - it's own unique connection / interaction
   console.log(socket.id);
 
-  // Bad event name atm but it pushes a new player to the store
-  socket.on("id", (player) => {
-    // Assign the socketId so that we can use it to remove the player if they leave
-    currentPlayers.push({ ...player, socketId: socket.id });
+  // Create Room
+  socket.on("create_room", async ({ uuid, ownerName, roomId, password }) => {
+    try {
+      console.log("room", { uuid, ownerName, roomId, password });
 
-    // Tell connected clients to update the player list.
-    io.emit("players update", currentPlayers);
+      const doesRoomExist = rooms.some((room) => room.roomId === roomId);
+      console.log({ doesRoomExist });
+      if (doesRoomExist) {
+        io.to(socket.id).emit(`${socket.id}-error`, {
+          message: "This room already exists!",
+        });
+      } else {
+        // Auto generate the password hash using bcrypt
+        const saltRounds = 10;
+        const passwordHash = await bcrypt.hash(password, saltRounds);
+
+        const newRoom = {
+          roomId,
+          password: passwordHash,
+          owner: {
+            uuid,
+            ownerName,
+            socketId: socket.id,
+          },
+          players: [],
+        };
+
+        rooms.push(newRoom);
+
+        console.log({ rooms });
+
+        io.to(socket.id).emit("room_update", newRoom);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+  // Join Room
+  socket.on(
+    "join_room",
+    async ({ uuid, characterName, initiativeRoll, roomId, password }) => {
+      try {
+        console.log("room id");
+
+        const roomIndex = rooms.findIndex((room) => roomId === room.roomId);
+        const room = rooms[roomIndex];
+
+        if (roomIndex !== -1) {
+          const validPassword = await bcrypt.compare(password, room.password);
+
+          if (validPassword) {
+            const newRoom = { ...room };
+
+            newRoom.players.push({
+              uuid,
+              characterName,
+              initiativeRoll,
+              socketId: socket.id,
+            });
+
+            rooms.splice(roomIndex, 1, newRoom);
+
+            io.to(newRoom.owner.socketId).emit("room_update", newRoom);
+
+            // User is now part of this room for the update, doesn't need it's own specific update.
+            newRoom.players.forEach((player) => {
+              io.to(player.socketId).emit("room_update", newRoom);
+            });
+          }
+        } else {
+          io.to(socket.id).emit(`${socket.id}-error`, {
+            message: "This room doesn't exist!",
+          });
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  );
+
+  // Leave Room
+  socket.on("leave_room", async ({ roomId }) => {
+    console.log("room id");
+
+    const roomIndex = rooms.findIndex((room) => roomId === room.roomId);
+    const room = rooms[roomIndex];
+
+    // Does the room exist anymore?
+    if (roomIndex !== -1) {
+      // Current connection is the room owner
+      if (room.owner.socketId === socket.id) {
+        let newRoom = { ...room };
+        const newOwner = newRoom.players[0];
+
+        // If there's a player left
+        if (newOwner) {
+          newRoom.players.splice(0, 1);
+          newRoom.owner = newOwner;
+          newRoom.owner.ownerName = newOwner.characterName;
+
+          rooms.splice(roomIndex, 1, newRoom);
+
+          console.log({ newRoom });
+
+          io.to(socket.id).emit("room_left");
+
+          io.to(newRoom.owner.socketId).emit("room_update", newRoom);
+
+          if (newRoom.players.length) {
+            newRoom.players.forEach((player) => {
+              io.to(player.socketId).emit("room_update", newRoom);
+            });
+          }
+        } else {
+          io.to(socket.id).emit("room_left");
+        }
+      } else {
+        // Is the player in the room?
+        const roomPlayerIndex = room.players.findIndex(
+          (player) => player.socketId === socket.id
+        );
+
+        if (roomPlayerIndex !== -1) {
+          const newRoom = { ...room };
+
+          newRoom.players.splice(roomPlayerIndex, 1);
+
+          rooms.splice(roomIndex, 1, newRoom);
+
+          io.to(socket.id).emit("room_left");
+
+          io.to(newRoom.owner.socketId).emit("room_update", newRoom);
+
+          newRoom.players.forEach((player) => {
+            io.to(player.socketId).emit("room_update", newRoom);
+          });
+        } else {
+          io.to(socket.id).emit(`${socket.id}-error`, {
+            message: "Something has gone wrong, you're not in a room?!",
+          });
+        }
+      }
+    } else {
+      io.to(socket.id).emit(`${socket.id}-error`, {
+        message: "Something has gone wrong, this room doesn't exist!",
+      });
+    }
   });
 
   // The client will automatically call the 'disconnect' event upon the `unload` event being triggered in a client
   socket.on("disconnect", (...something) => {
     // Remove the disconnected player, find via the socketId
+    console.log("player disconnected");
 
-    const playerIndex = currentPlayers.findIndex(
-      (player) => player.socketId === socket.id
-    );
-    currentPlayers.splice(playerIndex, 1);
+    const newRooms = rooms.map((room) => {
+      // Is the user in the room?
+      const roomPlayerIndex = room.players.findIndex(
+        (player) => player.socketId === socket.id
+      );
 
-    // Update the player list.
-    io.emit("players update", currentPlayers);
+      if (roomPlayerIndex !== -1) {
+        const newRoom = { ...room };
+
+        newRoom.players.splice(roomPlayerIndex, 1);
+
+        return newRoom;
+      }
+
+      return room;
+    });
+
+    // EMIT ROOMS?
+    // io.emit("players update", currentPlayers);
   });
 });
+
+io.listen(3001);
 
 server.listen(3000, () => {
   console.log("listening on *:3000");
--- fe/src/App.svelte
+++ fe/src/App.svelte
@@ -1,29 +1,55 @@
 <script>
-  import { v4 } from 'uuid'
+  import { v4 as uuidv4 } from 'uuid'
   import { io } from 'socket.io-client'
   import { afterUpdate, onMount } from 'svelte'
 
-  const uuid = v4()
+  const uuid = uuidv4()
   const socket = io('http://localhost:3000')
 
-  let currentPlayers = []
-  let playerName = ''
+  let socketId = null
+  let ownerName = ''
+  let roomId = ''
+  let password = ''
+
+  let characterName = ''
+  let initiativeRoll = ''
+
   let error = ''
+  let isSocketErrorSetup = false
+
+  let currentRoom = null
 
   // Lifecycle events all that jazz
   onMount(() => {
     console.log('app has mounted')
-  })
 
-  afterUpdate(() => {
-    console.log('after state update', { currentPlayers, playerName, error })
+    socket.on('connect', () => {
+      console.log('after connection', socket.id)
+      socketId = socket.id
+    })
   })
 
   // Setup the event for if the player list updates.
-  socket.on('players update', players => {
-    console.log('fe players update', players)
-    currentPlayers = players
-  })
+  // socket.on('players update', players => {
+  //   console.log('fe players update', players)
+  //   currentPlayers = players
+  // })
+
+  $: if (socketId) {
+    socket.on(`${socket.id}-error`, error => {
+      console.error(error)
+      error = error.message
+    })
+
+    socket.on('room_update', room => {
+      console.log('room update', room)
+      currentRoom = room
+    })
+
+    socket.on('room_left', () => {
+      currentRoom = null
+    })
+  }
 
   // This console logs in the event of ANYTHING happening with the client
   // Useful for debugging
@@ -31,23 +57,56 @@
   //   console.log('random log', event, args)
   // })
 
-  // Send an event to add a new user to the list of trackers
-  const onButtonPress = () => {
-    if (playerName) {
+  const createRoom = event => {
+    event.preventDefault()
+
+    if (!ownerName) error = 'No owner name'
+    else if (!roomId) error = 'No room id'
+    else if (!password) error = 'No room password'
+    else {
+      socket.emit('create_room', { uuid, ownerName, roomId, password })
       error = ''
-      socket.emit('id', { uuid, playerName })
-    } else {
-      error = 'you need a name'
+
+      // ownerName = ''
+      // roomId = ''
+      // password = ''
     }
+  }
+
+  // Send an event to add a new user to the list of trackers
+  const joinRoom = event => {
+    event.preventDefault()
+    if (!characterName) error = 'You need a name'
+    else if (!initiativeRoll) error = 'You need an initiative'
+    else if (!roomId) error = 'You need a room id'
+    else if (!password) error = 'No room password'
+    else {
+      socket.emit('join_room', {
+        uuid,
+        characterName,
+        initiativeRoll,
+        roomId,
+        password,
+      })
+      error = ''
+
+      // ownerName = ''
+      // roomId = ''
+      // password = ''
+    }
+  }
+
+  const leaveRoom = event => {
+    event.preventDefault()
+    socket.emit('leave_room', { roomId: currentRoom.roomId })
   }
 
   // Figure out if the user already exists in the player list
   // E.g. a refresh or a tab being re opened.
-  $: currentPlayersSocketIds = currentPlayers.map(player => player.socketId)
-  $: isUserInTracker = currentPlayersSocketIds.includes(socket.id)
+  // $: currentPlayersSocketIds = currentPlayers.map(player => player.socketId)
+  // $: isUserInTracker = currentPlayersSocketIds.includes(socket.id)
 
-  // Note here that we needed to do a $ in order to see this in the console logs to make it reactive
-  $: console.log({ currentPlayersSocketIds, id: socket.id })
+  $: console.log('socket connection', socketId)
 </script>
 
 <!-- Keep this for a talking point - kind of interesting -->
@@ -62,19 +121,82 @@
     <h2 style="color: red">{error}</h2>
   {/if}
 
-  {#if !isUserInTracker}
-    <input type="text" bind:value={playerName} placeholder="player name" />
+  {#if !currentRoom}
+    <form class="form" on:submit={createRoom}>
+      <input
+        class="input"
+        type="text"
+        bind:value={ownerName}
+        placeholder="Owner Name"
+      />
 
-    <button on:click={onButtonPress}>Join Tracker</button>
+      <input
+        class="input"
+        type="text"
+        bind:value={roomId}
+        placeholder="Room ID"
+      />
+
+      <input
+        class="input"
+        type="text"
+        bind:value={password}
+        placeholder="Password"
+      />
+
+      <input class="form-button" type="submit" value="Create room" />
+    </form>
+
+    <p>-----</p>
+
+    <form class="form" on:submit={joinRoom}>
+      <input
+        class="input"
+        type="text"
+        bind:value={characterName}
+        placeholder="Character name"
+      />
+
+      <input
+        class="input"
+        type="number"
+        bind:value={initiativeRoll}
+        placeholder="Initiative roll"
+      />
+
+      <input
+        class="input"
+        type="text"
+        bind:value={roomId}
+        placeholder="Room ID"
+      />
+
+      <input
+        class="input"
+        type="text"
+        bind:value={password}
+        placeholder="Password"
+      />
+
+      <input class="form-button" type="submit" value="Join tracker" />
+    </form>
   {/if}
 
-  <!-- Could also destructure here `as { playernName, uuid }` -->
-  {#each currentPlayers as player}
-    <div class="player-container">
-      <p class="player-name">{player.playerName}</p>
-      <p>{player.uuid}</p>
-    </div>
-  {/each}
+  {#if currentRoom}
+    <h3>{currentRoom.roomId} - {currentRoom.owner.ownerName}'s room</h3>
+
+    {#if !currentRoom.players.length} No players yet! {/if}
+
+    {#each currentRoom.players as player}
+      <div class="player-container">
+        <p class="player-name">{player.characterName}</p>
+        <p class="player-name">{player.initiativeRoll}</p>
+        <p>{player.uuid}</p>
+      </div>
+    {/each}
+
+    <button on:click={leaveRoom}>Leave room</button>
+  {/if}
 </main>
 
 <style>
@@ -89,5 +211,25 @@
     font-size: 18;
     color: orange;
     font-weight: bold;
+  }
+
+  .form {
+    display: flex;
+    flex-direction: column;
+    gap: 25px;
+    align-items: center;
+  }
+
+  .form-button {
+    padding: 10px 15px;
+    font-size: 12pt;
+    border-radius: 5px;
+  }
+
+  .input {
+    padding: 10px;
+    border-radius: 5px;
+    border-color: white;
+    font-size: 12pt;
   }
 </style>
